8d99529c0d44857ef5abf711d23c4d7f6b6dce50
Selyuu <rainierlouis@gmail.com>
2017-12-24 17:51:17 +0100

    style + feat: home screen styled + container functionality implemented

---
+++ app/screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from '../components/Container';
+
+export default () => (
+	<Container>
+		<View />
+	</Container>
+)
