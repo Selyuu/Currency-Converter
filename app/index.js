df4fa1db8afd68f3cad598f167d20087da8e4677
Selyuu <rainierlouis@gmail.com>
2017-12-28 17:08:54 +0100

    feat: alert provider component implemented

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Navigator from './config/routes';
+import { AlertProvider } from './components/Alert';
+
 
 EStyleSheet.build({
 	$primaryBlue: '#3e4982',
@@ -17,4 +19,4 @@ EStyleSheet.build({
 	$darkText: '#343434',
 })
 
-export default () => <Navigator />;
+export default () => <AlertProvider><Navigator /></AlertProvider>;
